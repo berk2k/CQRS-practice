1d59fc401d9cbcdbcd77f6078251eb33c6cc0f2b
Berk Polat <berkpolat125@gmail.com>
2024-08-18 17:27:38 +0300

    cqrs-mediatr example

---
+++ MediatR/MediatrExample/MediatrExample/Med/Commands/CreateProductCommand.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+
+namespace MediatrExample.Med.Commands
+{
+    public class CreateProductCommand : IRequest<Guid>
+    {
+        public string Name { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, Guid>
+        {
+            public Task<Guid> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+            {
+                //sending to db using repository
+                return Task.FromResult(Guid.NewGuid());
+            }
+        }
+    }
+}
---
+++ MediatR/MediatrExample/MediatrExample/Med/Queries/GetProductByIdQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+
+namespace MediatrExample.Med.Queries
+{
+    public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, GetProductViewModel>
+    {
+        public Task<GetProductViewModel> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            var viewModel = new GetProductViewModel()
+            {
+                Id = Guid.NewGuid(),
+                Name = "book"
+
+            };
+
+            return Task.FromResult(viewModel);
+            // get product from repository
+        }
+    }
+}
